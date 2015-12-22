68ae582bc3185b017515b0034677c45227b2c1b2
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-12-21 20:38:32 -0500

    WIP: First test of filter of builds (PHI #3936)
    Basic filtering of builds is complete.

---
+++ tribits/ci_support/ExtractCDashInfo.py
@@ -0,0 +1,124 @@
+# @HEADER
+# ************************************************************************
+#
+#            TriBITS: Tribal Build, Integrate, and Test System
+#                    Copyright 2013 Sandia Corporation
+#
+# Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+# the U.S. Government retains certain rights in this software.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the Corporation nor the names of the
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# ************************************************************************
+# @HEADER
+
+import urllib2
+import json
+import pprint
+from FindGeneralScriptSupport import *
+
+pp = pprint.PrettyPrinter()
+
+# Given a CDash query URL, return the full Python CDash data-structure
+def extractCDashApiQueryData(cdashApiQueryUrl):
+  response = urllib2.urlopen(cdashApiQueryUrl)
+  return json.load(response)
+
+
+# Given the full Python CDash API collapses builds data-structure, return an
+# reduced data-structure to be used for pass/fail examination.
+#
+# This function takes in the data-structre directly returned from:
+#
+#   <cdash-url>/api/vi/index.ph?project=<project>&date=<YYYY-MM-DD>&<filter-fields>
+#
+# The input full CDash API collapsed builds data-structure that has the
+# following structure and fields of interest:
+#
+#  fullCDashIndexBuilds =
+#  {
+#    'all_buildgroups': [ {'id':1,'name:"Nightly"}, ...],
+#    'buildgroups': [
+#      {
+#        'builds":[
+#          {
+#            'buildname':"???",
+#            'update': {'errors':???, ...},
+#            'configure':{'error': ???, ...},
+#            'compilation':{'error': ???, ...},
+#            'test': {'fail':???, 'notrun':???, 'pass':???, ...},
+#            ...
+#            },
+#            ...
+#          ]
+#        },
+#        ...
+#      ...
+#      ]
+#      },
+#      ...
+#    }
+#
+# This function gets the data from *all* of the collapsed builds and returns
+# the reduced data-structure:
+#
+#   [
+#     {
+#       'buildname':"???",
+#       'update': {'errors':???, ...},
+#       'configure':{'error': ???, ...},
+#       'compilation':{'error': ???, ...},
+#       'test': {'fail':???, 'notrun':???, 'pass':???, ...},
+#       ...
+#       },
+#       ...
+#       }
+#
+# This collects *all* of the builds from all of the build groups, not just the
+# 'Nighlty' build group.  Therefore, if you want to only consider on set of
+# build groups, you need to add that to the CDash query URL
+# (e.g. group='Nighlty').
+#
+def getCDashIndexBuildsSummary(fullCDashIndexBuilds):
+  summaryCDashIndexBuilds = []
+  for buildgroup in fullCDashIndexBuilds["buildgroups"]:
+    for build in buildgroup["builds"]:
+      summaryBuild = {
+        u'buildname' : build["buildname"],
+        u'update' : build['update'],
+        u'configure' : build['configure'],
+        u'compilation' : build['compilation'],
+        u'test' : build['test'],
+        }
+      summaryCDashIndexBuilds.append(summaryBuild)
+  return summaryCDashIndexBuilds
+  
+
+
+
+
