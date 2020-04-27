8b15410f3294cf4b4a9ffa7319444263e7dd3744
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-04-27 09:35:58 -0600

    WIP: Add TRIBITS_GET_DIR_ARRAY_BELOW_BASE_DIR() and unit tests (#314)
    I also fixed numerious mispelling of "UNITEST" to "UNITTEST".

--- tribits/core/utils/TribitsFilepathHelpers.cmake
+++ tribits/core/utils/TribitsFilepathHelpers.cmake
@@ -1,7 +1,10 @@
+INCLUDE(MessageWrapper)
+INCLUDE(Split)
+
 #
 # @FUNCTION: TRIBITS_DIR_IS_BASEDIR()
 #
-# Function to determin if a given path is a base dir of another path.
+# Function to determine if a given path is a base dir of another path.
 #
 # Usage::
 #
@@ -11,14 +14,14 @@
 # ``<absFullDir>``, then the variable ``<isBaseDirVarOut>`` is set to
 # ``TRUE``.  Otherwise, ``<isBaseDirVarOut>`` is set to ``FALSE``.
 #
-# For example, the ouptut var ``isBaseDir`` would be set to ``TRUE`` in the
+# For example, the output var ``isBaseDir`` would be set to ``TRUE`` in the
 # following examples::
 #
 #   TRIBITS_DIR_IS_BASEDIR(/some/base/path /some/base/path/more isBaseDir)
 #
 #   TRIBITS_DIR_IS_BASEDIR(/some/base/path /some/base/path isBaseDir)
 #
-# Hoever, in the following examples, ``isBaseDir`` would be set to ``FALSE``::
+# However, in the following examples, ``isBaseDir`` would be set to ``FALSE``::
 #
 #   TRIBITS_DIR_IS_BASEDIR(/some/base/path/more /some/base/path isBaseDir)
 #
@@ -42,5 +45,52 @@ FUNCTION(TRIBITS_DIR_IS_BASEDIR  absBaseDir  absFullDir  isBaseDirVarOut)
   ENDIF()
 
   SET(${isBaseDirVarOut} ${isBaseDir} PARENT_SCOPE)
+
+ENDFUNCTION()
+
+
+#
+# @FUNCTION: TRIBITS_GET_DIR_ARRAY_BELOW_BASE_DIR()
+#
+# Returns the array of directories below a base directory.
+#
+# Usage::
+#
+#  TRIBITS_GET_DIR_ARRAY_BELOW_BASE_DIR(<absBaseDir> <absFullDir>
+#     <trailingDirArrayVarOut>)
+#
+# The following show examples of what this returns:
+#
+#   TRIBITS_GET_DIR_ARRAY_BELOW_BASE_DIR("/a/b/c" "/a/b/c", dirArray)
+#     => dirArray = ""
+#
+#   TRIBITS_GET_DIR_ARRAY_BELOW_BASE_DIR("/a/b/c" "/a/b/c/d", dirArray)
+#     => dirArray = "d"
+#
+#   TRIBITS_GET_DIR_ARRAY_BELOW_BASE_DIR("/a/b/c" "/a/b/c/d/e", dirArray)
+#     => dirArray = "d;e"
+#
+FUNCTION(TRIBITS_GET_DIR_ARRAY_BELOW_BASE_DIR  absBaseDir  absFullDir
+  trailingDirArrayVarOut
+  )
+
+  TRIBITS_DIR_IS_BASEDIR("${absBaseDir}" "${absFullDir}" isBaseDir)
+  IF (NOT isBaseDir)
+    MESSAGE_WRAPPER(FATAL_ERROR
+      "ERROR: '${absBaseDir}' is not a base dir of '${absFullDir}'")
+  ENDIF()
+
+  STRING(LENGTH "${absBaseDir}" absBaseDirLen)
+  STRING(LENGTH "${absFullDir}" absFullDirLen)
+
+  IF (absBaseDirLen EQUAL absFullDirLen)
+    SET(trailingDirArray "")
+  ELSE()
+    MATH(EXPR trailingDirsStrStartIdx "${absBaseDirLen}+1")
+    STRING(SUBSTRING "${absFullDir}" ${trailingDirsStrStartIdx} -1 trailingDirsStr)
+    SPLIT("${trailingDirsStr}" "/" trailingDirArray)
+  ENDIF()
+
+  SET(${trailingDirArrayVarOut} "${trailingDirArray}" PARENT_SCOPE)
 
 ENDFUNCTION()
