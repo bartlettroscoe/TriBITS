9e2fa9452f11514c4d3d90231782df6766395933
Roscoe A. Bartlett <rabartl@sandia.gov>
2021-06-24 09:13:37 -0600

    WIP: Break out some code as functions

--- tribits/doc/developers_guide/generate-dev-guide.sh
+++ tribits/doc/developers_guide/generate-dev-guide.sh
@@ -57,6 +57,12 @@ ARGS=$@
 
 source ../utils/gen_doc_utils.sh
 
+
+#
+# Functions
+#
+
+
 function generate_gitdist_dist_help_topic {
   help_topic_name=$1
   ../../python_utils/gitdist --dist-help=$help_topic_name &> gitdist-dist-help-$help_topic_name.txt.tmp
@@ -64,130 +70,148 @@ function generate_gitdist_dist_help_topic {
 }
 
 
-if [ "$TRIBITS_DEV_GUIDE_SKIP_DOCUMENTATION_EXTRACTION" == "" ] ; then
+function tribits_extract_rst_cmake_doc {
+  extra_args=$1
 
-  echo
-  echo "Extracting TriBITS documentation from *.cmake files ..."
-  echo
-  ../../python_utils/extract_rst_cmake_doc.py \
-    --extract-from=../../core/package_arch/,../../ci_support/,../../core/utils/,../../ctest_driver/ \
-    --rst-file-pairs=TribitsMacroFunctionDocTemplate.rst:TribitsMacroFunctionDoc.rst.tmp,UtilsMacroFunctionDocTemplate.rst:UtilsMacroFunctionDoc.rst.tmp,TribitsSystemMacroFunctionDocTemplate.rst:TribitsSystemMacroFunctionDoc.rst.tmp \
-    --show-file-name-line-num \
-    --file-name-path-base-dir=../.. \
-    $TRIBITS_DEV_GUIDE_EXTRACT_RST_CMAKE_DOC_EXTRA_ARGS
+  if [ "$TRIBITS_DEV_GUIDE_SKIP_DOCUMENTATION_EXTRACTION" == "" ] ; then
 
-  update_if_different  TribitsMacroFunctionDoc.rst  tmp
-  update_if_different  UtilsMacroFunctionDoc.rst  tmp
-  update_if_different  TribitsSystemMacroFunctionDoc.rst  tmp
+    echo
+    echo "Extracting TriBITS documentation from *.cmake files ..."
+    echo
+    ../../python_utils/extract_rst_cmake_doc.py \
+      --extract-from=../../core/package_arch/,../../ci_support/,../../core/utils/,../../ctest_driver/ \
+      --rst-file-pairs=TribitsMacroFunctionDocTemplate.rst:TribitsMacroFunctionDoc.rst.tmp,UtilsMacroFunctionDocTemplate.rst:UtilsMacroFunctionDoc.rst.tmp,TribitsSystemMacroFunctionDocTemplate.rst:TribitsSystemMacroFunctionDoc.rst.tmp \
+      ${extra_args} \
+      --file-name-path-base-dir=../.. \
+      $TRIBITS_DEV_GUIDE_EXTRACT_RST_CMAKE_DOC_EXTRA_ARGS
 
-fi
+    update_if_different  TribitsMacroFunctionDoc.rst  tmp
+    update_if_different  UtilsMacroFunctionDoc.rst  tmp
+    update_if_different  TribitsSystemMacroFunctionDoc.rst  tmp
 
-if [ "$TRIBITS_DEV_GUIDE_SKIP_OTHER_EXTRACTION" == "" ] ; then
+  fi
 
-  echo
-  echo "Generating list of Standard TriBITS TPLs ..."
-  echo
-  ls -w 1 ../../core/std_tpls/ &> TribitsStandardTPLsList.txt.tmp
-  update_if_different  TribitsStandardTPLsList.txt  tmp
+}
 
-  echo
-  echo "Generating list of Common TriBITS TPLs ..."
-  echo
-  ls -w 1 ../../common_tpls/ &> TribitsCommonTPLsList.txt.tmp
-  update_if_different  TribitsCommonTPLsList.txt  tmp
+function tribits_extract_other_doc {
 
-  echo
-  echo "Generating Directory structure of TribitsHelloWorld ..."
-  echo
-  ../../python_utils/tree.py -f -c -x ../../examples/TribitsHelloWorld/ \
-    &> TribitsHelloWorldDirAndFiles.txt.tmp
-  update_if_different  TribitsHelloWorldDirAndFiles.txt  tmp
+  if [ "$TRIBITS_DEV_GUIDE_SKIP_OTHER_EXTRACTION" == "" ] ; then
 
-  echo
-  echo "Generating output for 'checkin-test.py --help' ..."
-  echo
-  ../../ci_support/checkin-test.py --help &> checkin-test-help.txt.tmp
-  update_if_different  checkin-test-help.txt  tmp
+    echo
+    echo "Generating list of Standard TriBITS TPLs ..."
+    echo
+    ls -w 1 ../../core/std_tpls/ &> TribitsStandardTPLsList.txt.tmp
+    update_if_different  TribitsStandardTPLsList.txt  tmp
 
-  echo
-  echo "Generating output for 'gitdist --help' and '--dist-help=<topic>' ..."
-  echo
-  ../../python_utils/gitdist --help &> gitdist-help.txt.tmp
-  update_if_different  gitdist-help.txt  tmp
-  generate_gitdist_dist_help_topic overview
-  generate_gitdist_dist_help_topic repo-selection-and-setup
-  generate_gitdist_dist_help_topic dist-repo-status
-  generate_gitdist_dist_help_topic repo-versions
-  generate_gitdist_dist_help_topic aliases
-  generate_gitdist_dist_help_topic default-branch
-  generate_gitdist_dist_help_topic move-to-base-dir
-  generate_gitdist_dist_help_topic usage-tips
-  generate_gitdist_dist_help_topic script-dependencies
-  generate_gitdist_dist_help_topic all
+    echo
+    echo "Generating list of Common TriBITS TPLs ..."
+    echo
+    ls -w 1 ../../common_tpls/ &> TribitsCommonTPLsList.txt.tmp
+    update_if_different  TribitsCommonTPLsList.txt  tmp
 
-  echo
-  echo "Generating output for 'clone_extra_repos.py --help' ..."
-  echo
-  ../../ci_support/clone_extra_repos.py --help \
-    &> clone_extra_repos-help.txt.tmp
-  update_if_different  clone_extra_repos-help.txt  tmp
+    echo
+    echo "Generating Directory structure of TribitsHelloWorld ..."
+    echo
+    ../../python_utils/tree.py -f -c -x ../../examples/TribitsHelloWorld/ \
+      &> TribitsHelloWorldDirAndFiles.txt.tmp
+    update_if_different  TribitsHelloWorldDirAndFiles.txt  tmp
 
-  echo
-  echo "Generating output for 'snapshot-dir.py --help' ..."
-  echo
-  env SNAPSHOT_DIR_DUMMY_DEFAULTS=1 ../../python_utils/snapshot-dir.py --help \
-   &> snapshot-dir-help.txt.tmp
-  update_if_different  snapshot-dir-help.txt  tmp
+    echo
+    echo "Generating output for 'checkin-test.py --help' ..."
+    echo
+    ../../ci_support/checkin-test.py --help &> checkin-test-help.txt.tmp
+    update_if_different  checkin-test-help.txt  tmp
 
-  echo
-  echo "Generating output for 'is_checkin_tested_commit.py --help' ..."
-  echo
-  ../../ci_support/is_checkin_tested_commit.py --help \
-    &> is_checkin_tested_commit.txt.tmp
-  update_if_different  is_checkin_tested_commit.txt  tmp
+    echo
+    echo "Generating output for 'gitdist --help' and '--dist-help=<topic>' ..."
+    echo
+    ../../python_utils/gitdist --help &> gitdist-help.txt.tmp
+    update_if_different  gitdist-help.txt  tmp
+    generate_gitdist_dist_help_topic overview
+    generate_gitdist_dist_help_topic repo-selection-and-setup
+    generate_gitdist_dist_help_topic dist-repo-status
+    generate_gitdist_dist_help_topic repo-versions
+    generate_gitdist_dist_help_topic aliases
+    generate_gitdist_dist_help_topic default-branch
+    generate_gitdist_dist_help_topic move-to-base-dir
+    generate_gitdist_dist_help_topic usage-tips
+    generate_gitdist_dist_help_topic script-dependencies
+    generate_gitdist_dist_help_topic all
 
-  echo
-  echo "Generating output for 'get-tribits-packages-from-files-list.py --help' ..."
-  echo
-  ../../ci_support/get-tribits-packages-from-files-list.py --help \
-    &> get-tribits-packages-from-files-list.txt.tmp
-  update_if_different  get-tribits-packages-from-files-list.txt  tmp
+    echo
+    echo "Generating output for 'clone_extra_repos.py --help' ..."
+    echo
+    ../../ci_support/clone_extra_repos.py --help \
+      &> clone_extra_repos-help.txt.tmp
+    update_if_different  clone_extra_repos-help.txt  tmp
 
-  echo
-  echo "Generating output for 'get-tribits-packages-from-last-tests-failed.py --help' ..."
-  echo
-  ../../ci_support/get-tribits-packages-from-last-tests-failed.py --help \
-    &> get-tribits-packages-from-last-tests-failed.txt.tmp
-  update_if_different  get-tribits-packages-from-last-tests-failed.txt  tmp
+    echo
+    echo "Generating output for 'snapshot-dir.py --help' ..."
+    echo
+    env SNAPSHOT_DIR_DUMMY_DEFAULTS=1 ../../python_utils/snapshot-dir.py --help \
+     &> snapshot-dir-help.txt.tmp
+    update_if_different  snapshot-dir-help.txt  tmp
 
-  echo
-  echo "Generating output for 'filter-packages-list.py --help' ..."
-  echo
-  ../../ci_support/filter-packages-list.py --help \
-    &> filter-packages-list.txt.tmp
-  update_if_different  filter-packages-list.txt  tmp
+    echo
+    echo "Generating output for 'is_checkin_tested_commit.py --help' ..."
+    echo
+    ../../ci_support/is_checkin_tested_commit.py --help \
+      &> is_checkin_tested_commit.txt.tmp
+    update_if_different  is_checkin_tested_commit.txt  tmp
 
-  echo
-  echo "Generating output for 'install_devtools.py --help' ..."
-  echo
-  ../../devtools_install/install_devtools.py --help \
-    &> install_devtools-help.txt.tmp
-  update_if_different  install_devtools-help.txt  tmp
+    echo
+    echo "Generating output for 'get-tribits-packages-from-files-list.py --help' ..."
+    echo
+    ../../ci_support/get-tribits-packages-from-files-list.py --help \
+      &> get-tribits-packages-from-files-list.txt.tmp
+    update_if_different  get-tribits-packages-from-files-list.txt  tmp
 
-fi
+    echo
+    echo "Generating output for 'get-tribits-packages-from-last-tests-failed.py --help' ..."
+    echo
+    ../../ci_support/get-tribits-packages-from-last-tests-failed.py --help \
+      &> get-tribits-packages-from-last-tests-failed.txt.tmp
+    update_if_different  get-tribits-packages-from-last-tests-failed.txt  tmp
 
-if [ -e "../../../README.DIRECTORY_CONTENTS.rst" ] ; then
-  echo
-  echo "Copy TriBITS/README.DIRECTORY_CONTENTS.rst to TriBITS.README.DIRECTORY_CONTENTS.rst ..."
-  echo
-  cp ../../../README.DIRECTORY_CONTENTS.rst TriBITS.README.DIRECTORY_CONTENTS.rst.tmp
-else
-  echo
-  echo "TriBITS/README.DIRECTORY_CONTENTS.rst does not exist to copy!"
-  echo
-  touch TriBITS.README.DIRECTORY_CONTENTS.rst.tmp
-fi
-update_if_different  TriBITS.README.DIRECTORY_CONTENTS.rst  tmp
+    echo
+    echo "Generating output for 'filter-packages-list.py --help' ..."
+    echo
+    ../../ci_support/filter-packages-list.py --help \
+      &> filter-packages-list.txt.tmp
+    update_if_different  filter-packages-list.txt  tmp
+
+    echo
+    echo "Generating output for 'install_devtools.py --help' ..."
+    echo
+    ../../devtools_install/install_devtools.py --help \
+      &> install_devtools-help.txt.tmp
+    update_if_different  install_devtools-help.txt  tmp
+
+  fi
+
+  if [ -e "../../../README.DIRECTORY_CONTENTS.rst" ] ; then
+    echo
+    echo "Copy TriBITS/README.DIRECTORY_CONTENTS.rst to TriBITS.README.DIRECTORY_CONTENTS.rst ..."
+    echo
+    cp ../../../README.DIRECTORY_CONTENTS.rst TriBITS.README.DIRECTORY_CONTENTS.rst.tmp
+  else
+    echo
+    echo "TriBITS/README.DIRECTORY_CONTENTS.rst does not exist to copy!"
+    echo
+    touch TriBITS.README.DIRECTORY_CONTENTS.rst.tmp
+  fi
+  update_if_different  TriBITS.README.DIRECTORY_CONTENTS.rst  tmp
+
+}
+
+
+#
+# Executable code
+#
+
+tribits_extract_rst_cmake_doc --show-file-name-line-num
+
+tribits_extract_other_doc
 
 generate_git_version_file
 
